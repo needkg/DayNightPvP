f64048022e98a4502fa30fa999c81c6c08d2dca1
needkg <juniormartins260203.m.o@gmail.com>
2023-12-29 19:11:41 -0300

    remove unnecessary code

--- src/main/java/org/callvdois/daynightpvp/events/RegisterEvents.java
+++ src/main/java/org/callvdois/daynightpvp/events/RegisterEvents.java
@@ -33,11 +33,6 @@ public class RegisterEvents {
     }
 
     private void registerEntityEvent() {
-        //if (PluginUtils.isPluginInstalled("GriefPrevention")) {
-        //    if (!ConfigManager.griefPreventionPvpInLand) {
-        //        pluginManager.registerEvents(new DamageEvent(), DayNightPvP.plugin);
-        //    }
-        //}
         pluginManager.registerEvents(new DamageEvent(), DayNightPvP.getInstance());
     }
 
