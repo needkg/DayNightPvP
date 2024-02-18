8704c6e426abb8e100990e1ae89f10607afe5d74
needkg <juniormartins260203.m.o@gmail.com>
2024-02-17 22:40:40 -0300

    fixes an issue with placeholder support

--- src/main/java/org/callvdois/daynightpvp/placeholder/PvpStatus.java
+++ src/main/java/org/callvdois/daynightpvp/placeholder/PvpStatus.java
@@ -5,15 +5,19 @@ import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.callvdois.daynightpvp.DayNightPvP;
+import org.callvdois.daynightpvp.config.ConfigManager;
 import org.callvdois.daynightpvp.config.LangManager;
+import org.callvdois.daynightpvp.utils.SearchUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class PvpStatus extends PlaceholderExpansion {
 
     private final LangManager langManager;
+    private final ConfigManager configManager;
 
     public PvpStatus() {
         langManager = new LangManager();
+        configManager = new ConfigManager();
     }
 
 
@@ -41,19 +45,31 @@ public class PvpStatus extends PlaceholderExpansion {
     public String onPlaceholderRequest(Player player, String params) {
 
         if (params.equalsIgnoreCase("current_world_pvpstatus")) {
-            boolean pvpEnabled = player.getWorld().getPVP();
-            return pvpEnabled ? langManager.getString("placeholder-pvp-enabled") : langManager.getString("placeholder-pvp-disabled");
+            boolean pvpStatus;
+
+            World world = player.getWorld();
+            if (SearchUtils.stringExistInList(configManager.getList("daynightpvp.worlds"), world.getName())) {
+                long time = world.getTime();
+                pvpStatus = time >= configManager.getInt("daynightpvp.day-end");
+            } else {
+                return langManager.getString("feedback-error");
+            }
+            return pvpStatus ? langManager.getString("placeholder-pvp-enabled") : langManager.getString("placeholder-pvp-disabled");
         }
 
         if (params.startsWith("pvpstatus_")) {
+            boolean pvpStatus;
             String worldName = params.substring("pvpstatus_".length());
             World world = Bukkit.getWorld(worldName);
             if (world != null) {
-                boolean pvpEnabled = world.getPVP();
-                return pvpEnabled ? langManager.getString("placeholder-pvp-enabled") : langManager.getString("placeholder-pvp-disabled");
+                if (SearchUtils.stringExistInList(configManager.getList("daynightpvp.worlds"), world.getName())) {
+                    long time = world.getTime();
+                    pvpStatus = time >= configManager.getInt("daynightpvp.day-end");
+                    return pvpStatus ? langManager.getString("placeholder-pvp-enabled") : langManager.getString("placeholder-pvp-disabled");
+                }
             }
         }
-        return null;
+        return langManager.getString("feedback-error");
     }
 
 }
