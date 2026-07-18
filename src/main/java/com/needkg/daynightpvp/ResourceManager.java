d8f06460ba82aba6a431d41f6bc11e50fbdf505d
jhonatapers <jhonatapers@gmail.com>
2026-07-18 18:54:16 -0300

    refactor: standardize resource path handling and improve YAML loader methods
    Co-authored-by: Claudinei Martins de Oliveira Junior <juniormartins260203.m.o@gmail.com>

--- src/main/java/com/needkg/daynightpvp/ResourceManager.java
+++ src/main/java/com/needkg/daynightpvp/ResourceManager.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
 
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -18,7 +19,7 @@ import com.needkg.daynightpvp.util.FileUtils;
 public class ResourceManager {
 
     private final static String CONFIG_YML = "config.yml";
-    private final static String WORLDS = "worlds.yml";
+    private final static String WORLDS_YML = "worlds.yml";
     private final static String DEFAULT_LANG_YML = "lang/en.yml";
 
     private final JavaPlugin plugin;
@@ -28,24 +29,24 @@ public class ResourceManager {
     }
 
     public ResourceLoader initializeConfigYml() {
-        return initializeYamlLoader(plugin, Path.of(CONFIG_YML), false, null);
+        return initializeYamlLoader(plugin, CONFIG_YML, false);
     }
 
     public ResourceLoader initializeLanguageYml(String lang) {
+
         Path langPath = Path.of("lang", lang + ".yml");
         final var defaultLang = new ResourceLoaderYaml(new InputStreamReader(plugin.getResource(DEFAULT_LANG_YML)));
-        return initializeYamlLoader(plugin, langPath, false, defaultLang);
+        return initializeYamlLoaderLanguage(plugin, langPath.toString(), false, defaultLang);
     }
 
     public ResourceLoader initializeWorldsYml() {
-        return initializeYamlLoader(plugin, Path.of(WORLDS), false, null);
+        return initializeYamlLoader(plugin, WORLDS_YML, false);
     }
 
     private ResourceLoader initializeYamlLoader(
             JavaPlugin plugin,
-            Path resourcePath,
-            boolean reset,
-            ResourceLoader defaultResourceLoader) {
+            String resourcePath,
+            boolean reset) {
 
         File file = new File(plugin.getDataFolder(), resourcePath.toString());
 
@@ -54,9 +55,31 @@ public class ResourceManager {
         }
 
         return new ResourceLoaderYaml(
-                FileUtils.openReader(plugin.getDataPath().resolve(resourcePath)),
-                defaultResourceLoader);
+                FileUtils.openReader(plugin.getDataPath().resolve(resourcePath)), null);
 
+    }
+
+    private ResourceLoader initializeYamlLoaderLanguage(
+            JavaPlugin plugin,
+            String resourcePath,
+            boolean reset,
+            ResourceLoader defaultResourceLoader) {
+
+        final var optionalLanguageResource = Optional.of(
+                new ResourceLoaderYaml(
+                        new InputStreamReader(plugin.getResource(resourcePath))));
+
+        final var filePath = optionalLanguageResource.isPresent() ? resourcePath.toString() : DEFAULT_LANG_YML;
+
+        File file = new File(plugin.getDataFolder(), filePath);
+
+        if (!file.exists() || reset) {
+            plugin.saveResource(resourcePath.toString(), reset);
+        }
+
+        return new ResourceLoaderYaml(
+                FileUtils.openReader(plugin.getDataPath().resolve(file.getPath())),
+                defaultResourceLoader);
     }
 
 }
